c8071770df0978fb58f68682086c3b3a23d6d3d7
Abrahamdom <abrahamdom.sap@gmail.com>
2021-06-22 03:59:08 +0000

    2.3 Lista desplegable

--- Employees/webapp/controller/App.controller.js
+++ Employees/webapp/controller/App.controller.js
@@ -7,9 +7,21 @@ sap.ui.define([
 	function (Controller) {
 		"use strict";
 
-		return Controller.extend("abrahamgroup.Employees.controller.App", {
-			onInit: function () {
-
-			}
-		});
+		var Main = Controller.extend("abrahamgroup.Employees.controller.App", {});
+            
+        Main.prototype.onValidate = function() {
+                                        var inputEmployee = this.byId("inputEmployee");
+                                        var valueEmployee = inputEmployee.getValue();
+                                    
+                                        if(valueEmployee.length === 6){
+                                            //inputEmployee.setDescription("Ok");
+                                            this.byId("labelCountry").setVisible(true);
+                                            this.byId("slCountry").setVisible(true);
+                                        }else{
+                                            //inputEmployee.setDescription("Not Ok");
+                                            this.byId("labelCountry").setVisible(false);
+                                            this.byId("slCountry").setVisible(false);
+                                        }
+                                    };
+        return Main;
 	});
