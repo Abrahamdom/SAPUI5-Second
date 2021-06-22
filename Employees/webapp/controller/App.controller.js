7f665e36a38c713cab0e42d51be48969fffd6280
Abrahamdom <abrahamdom.sap@gmail.com>
2021-06-22 04:04:23 +0000

    2.3 Lista desplegable

--- Employees/webapp/controller/App.controller.js
+++ Employees/webapp/controller/App.controller.js
@@ -8,13 +8,17 @@ sap.ui.define([
 		"use strict";
 
 		var Main = Controller.extend("abrahamgroup.Employees.controller.App", {});
-            
+        
+        //Prototipamos la función onValidate dentro del main y la regresamos con return.
         Main.prototype.onValidate = function() {
+                                        //this (contexto de la vista).byId(llamamos con base en el id)
                                         var inputEmployee = this.byId("inputEmployee");
+                                        //Obtenemos el valor del input con getValue
                                         var valueEmployee = inputEmployee.getValue();
                                     
                                         if(valueEmployee.length === 6){
                                             //inputEmployee.setDescription("Ok");
+                                            //this(Contexto de la vista).setVisible(establecemos un valor en la propiedad visible)
                                             this.byId("labelCountry").setVisible(true);
                                             this.byId("slCountry").setVisible(true);
                                         }else{
