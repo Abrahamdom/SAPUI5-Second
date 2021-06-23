d76c38c7731fe3cab30ce9dce6bdcd7935254916
Abrahamdom <abrahamdom.sap@gmail.com>
2021-06-23 02:48:46 +0000

    Contextos y mensajes

--- Employees/webapp/controller/App.controller.js
+++ Employees/webapp/controller/App.controller.js
@@ -62,6 +62,26 @@ sap.ui.define([
             oModel.setProperty("/EmployeeId", "");
             oModel.setProperty("/CountryKey", "");
         };
+
+        //Para recibir el objeto del evento, tenemos que poner dentro del parentesis de la funcion "oEvent"
+        function showPostalCode(oEvent){
+            
+            //Esta función nos va a indicar cual botón pulso el usuario, para ésto tenemos que obtener su objeto JSON para identificar cual presionó.
+
+            //Del evento obtenemos con getSource el valor actual de la fuente de la propiedad, es decir, el botón que fué presionado
+            var itemPressed = oEvent.getSource();
+            //getBindingContext se usa para obtener el contexto de enlace de éste objeto para el nombre de modelo dado
+            var oContext = itemPressed.getBindingContext();
+            //getObject -> Devuelve un objeto JSON que es una copia de los datos de la entidad a los que hace referencia el sPath y oContext.
+            //getObject ( sPath , oContext ?, mParameters ?)
+            //Es decir, para obtener el objeto en contexto, primero tenemos que obtene la fuente del objeto, luego el contexto del objeto para después obtener el objeto JSON
+            var objectContext = oContext.getObject();
+
+            //Con MessageToast.show() mostramos un mensaje de tipo toast, dentro de los parentesis colocamos el objeto JSON que obtuvimos y le agregamos la propiedad PostalCode
+            //  para que nos muestre esa propiedad en el mensaje.
+            sap.m.MessageToast.show(objectContext.PostalCode);
+
+        };
             
         var Main = Controller.extend("abrahamgroup.Employees.controller.App", {});
         
@@ -88,6 +108,7 @@ sap.ui.define([
         Main.prototype.onInit = onInit;
         Main.prototype.onFilter = onFilter;
         Main.prototype.onClearFilter = onClearFilter;
+        Main.prototype.showPostalCode = showPostalCode;                            
 
         return Main;
 	});
